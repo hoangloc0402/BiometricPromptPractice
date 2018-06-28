704b030ad499a68dce2d0e547d90547d0ac39e20
Hoang Loc <1511849@hcmut.edu.vn>
2018-06-28 16:39:53 +0700

    Add deprecated FingerprintManager

---
+++ app/src/main/java/com/hoangloc/biometricsprompt/ActivityFingerprintManager.java
@@ -0,0 +1,136 @@
+package com.hoangloc.biometricsprompt;
+
+import android.Manifest;
+import android.os.Bundle;
+import java.io.IOException;
+import javax.crypto.Cipher;
+import android.widget.Toast;
+import javax.crypto.SecretKey;
+import java.security.KeyStore;
+import javax.crypto.KeyGenerator;
+import android.app.KeyguardManager;
+import java.security.KeyStoreException;
+import android.content.pm.PackageManager;
+import java.security.InvalidKeyException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.NoSuchProviderException;
+import android.support.v4.app.ActivityCompat;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import android.security.keystore.KeyProperties;
+import android.support.v7.app.AppCompatActivity;
+import android.security.keystore.KeyGenParameterSpec;
+import android.hardware.fingerprint.FingerprintManager;
+import java.security.InvalidAlgorithmParameterException;
+import android.security.keystore.KeyPermanentlyInvalidatedException;
+
+
+
+public class ActivityFingerprintManager extends AppCompatActivity {
+    private static final String KEY_NAME = "test_key";
+    private FingerprintManager fingerprintManager;
+    private FingerprintManager.CryptoObject cryptoObject;
+    private KeyguardManager keyguardManager;
+    private KeyStore keyStore;
+    private KeyGenerator keyGenerator;
+    private Cipher cipher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fingerprint_manager);
+        fingerprintManager = (FingerprintManager) getSystemService(FINGERPRINT_SERVICE);
+        keyguardManager = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
+
+        if (hasSecuritySettingError()) return;
+
+        getKeyStoreInstance();
+        getKeyGeneratorInstance();
+        generateKey();
+
+        if (cipherInit()) {
+            cryptoObject = new FingerprintManager.CryptoObject(cipher);
+            FingerprintHandler helper = new FingerprintHandler(this);
+            helper.startAuth(fingerprintManager, cryptoObject);
+        }
+    }
+
+    private boolean hasSecuritySettingError() {
+        if (!keyguardManager.isKeyguardSecure()) {
+            Toast.makeText(this, "Lock screen security not enabled in Settings", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Fingerprint authentication permission not enabled", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        if (!fingerprintManager.hasEnrolledFingerprints()) {
+            Toast.makeText(this, "Register at least one fingerprint in Settings", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return false;
+    }
+
+    private void getKeyStoreInstance() {
+        try {
+            keyStore = KeyStore.getInstance("AndroidKeyStore");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getKeyGeneratorInstance() {
+        try {
+            keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore");
+        } catch (NoSuchAlgorithmException | NoSuchProviderException e) {
+            throw new RuntimeException(
+                    "Failed to get KeyGenerator instance", e);
+        }
+    }
+
+    private void generateKey() {
+        try {
+            keyStore.load(null);
+            keyGenerator.init(
+                    new KeyGenParameterSpec.Builder(KEY_NAME, KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
+                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                    .setUserAuthenticationRequired(true)
+                    .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                    .build());
+            keyGenerator.generateKey();
+        } catch (NoSuchAlgorithmException
+                | InvalidAlgorithmParameterException
+                | CertificateException
+                | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private boolean cipherInit() {
+        try {
+            cipher = Cipher.getInstance(
+                            KeyProperties.KEY_ALGORITHM_AES + "/"
+                            + KeyProperties.BLOCK_MODE_CBC + "/"
+                            + KeyProperties.ENCRYPTION_PADDING_PKCS7);
+        }
+        catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            throw new RuntimeException("Failed to get Cipher", e);
+        }
+
+        try {
+            keyStore.load(null);
+            SecretKey key = (SecretKey) keyStore.getKey(KEY_NAME, null);
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            return true;
+        }
+        catch (KeyPermanentlyInvalidatedException e) {
+            return false;
+        }
+        catch (KeyStoreException | CertificateException
+                | UnrecoverableKeyException | IOException
+                | NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new RuntimeException("Failed to init Cipher", e);
+        }
+    }
+}
